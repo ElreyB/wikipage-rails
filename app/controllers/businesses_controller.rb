50910a739017dcba8a1ee5880175d2fb62c7b852
Elrey Belmonti and Megan Olson <elrey+megan@>
2017-11-13 11:54:53 -0800

    Add, update, and delete businesses.

---
+++ app/controllers/businesses_controller.rb
@@ -0,0 +1,52 @@
+class BusinessesController < ApplicationController
+
+  def new
+    @type = Type.find(params[:type_id])
+    @business = @type.businesses.new
+  end
+
+  def show
+    @business = Business.find(params[:id])
+    render :show
+  end
+
+  def create
+    @type = Type.find(params[:type_id])
+    @business = @type.businesses.new(business_params)
+    if @business.save
+      redirect_to type_path(@type)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @type = Type.find(params[:type_id])
+    @business = Business.find(params[:id])
+    if @business.update(business_params)
+      redirect_to type_path(@type)
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @type = Type.find(params[:type_id])
+    @business = Business.find(params[:id])
+    render :edit
+  end
+
+  def destroy
+    @type = Type.find(params[:type_id])
+    @business = Business.find(params[:id])
+    @business.destroy
+    redirect_to type_path(@type)
+  end
+
+private
+
+  def business_params
+    params.require(:business).permit(:name, :address, :phone_number, :website, :open_at, :closed_at)
+  end
+
+end
